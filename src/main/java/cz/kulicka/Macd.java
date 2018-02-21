1a4c47e5d1693e89b75ed70fef89cdca972d5f5d
Michal Kasl <mikasl@csob.cz>
2018-02-21 18:06:35 +0100

    macd

---
+++ src/main/java/cz/kulicka/Macd.java
@@ -0,0 +1,98 @@
+package cz.kulicka;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.kulicka.entity.Kline;
+import cz.kulicka.utils.MapperUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Macd {
+
+//    https://github.com/sergiocormio/stock-alerts/
+//    http://www.iexplain.org/ema-how-to-calculate/.
+//    https://www.investujeme.cz/clanky/macd-temer-svaty-gral/
+
+
+    public static void main(String[] args) {
+
+        File jsonFile = new File("src/test/resources/klines.json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ArrayList<ArrayList<String>> result = null;
+        try {
+            result = objectMapper.readValue(jsonFile, new TypeReference<ArrayList<ArrayList<String>>>() {
+            });
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Kline> klines = MapperUtil.klinesJsonArrayToKlinesObjectArray(result);
+
+        Collections.reverse(klines);
+
+
+
+
+//        MACD = EMA (12) – EMA (26)
+//
+//        Pro jasné znázornění nákupních a prodejních příkazů se používá ještě třetí exponenciální klouzavý průměr s délkou 9 období, který tvoří tzv. signální křivku.
+//
+//        Signal = EMA (9)
+//
+//        Samotný indikátor výrazně zdokonalil Thomas Aspray v roce 1986, kdy znázornil indikátor MACD ve formě histogramu. Výpočet je jednoduchý
+//
+//        Histogram = MACD – signal
+
+
+        ArrayList<Float> emaShort = new ArrayList<>();
+
+        ArrayList<Float> emaLong = new ArrayList<>();
+
+        Macd v = new Macd();
+        v.emaCalc(emaShort, klines, 12);
+
+        v.emaCalc(emaLong, klines, 26);
+
+        ArrayList<Float> macdik = new ArrayList<>();
+
+        for (int i = 0; emaLong.size() > i; i++){
+            macdik.add(emaShort.get(i) - emaLong.get(i));
+        }
+
+
+        System.out.print("sdf");
+    }
+
+    public float CalculateEMA(float todaysPrice, float numberOfDays, float EMAYesterday){
+        float k = 2 / (numberOfDays + 1);
+        return todaysPrice * k + EMAYesterday * (1 - k);
+    }
+
+    public void emaCalc(ArrayList<Float> emaList, ArrayList<Kline> klines, float days){
+
+        float ema;
+        float yesterdayEMA = 0;
+
+        for (Kline kline: klines){
+            //call the EMA calculation
+            ema = CalculateEMA((float)kline.getClose(), days, yesterdayEMA);
+            //put the calculated ema in an array
+            emaList.add(ema);
+            //make sure yesterdayEMA gets filled with the EMA we used this time around
+            yesterdayEMA = ema;
+
+
+        }
+    }
+}
